55305606531e4ec885c9d4e489a766d40655e529
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2025-01-01 15:45:36 +0100

    feat: fixing volumes

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 echo "Setting permissions for mysql..."
 chown -R mysql:mysql /var/lib/mysql
 
+echo "Updating bind-address to 0.0.0.0..."
+sed -i 's/bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
 exec "$@"
