96f18c7fac04c330c40d8eba85f9efcc66d23a12
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2025-01-02 11:51:37 +0100

    generate ssl-certificates directly in nginx-container

--- srcs/requirements/nginx/conf/ssl/create_ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-       -keyout inception-nginx-selfsigned.key \
-       -out inception-nginx-selfsigned.crt
-
-# to view contents of cert: openssl x509 -in inception-nginx-selfsigned.crt -text -noout
