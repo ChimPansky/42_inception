f8ff8850e8971b6fd2d676ca15a3f1536a12521e
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2025-01-03 12:12:22 +0100

    fix: mariadb entry script root pw

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 echo "Starting Mariadb..."
 
-echo "Updating bind-address to 0.0.0.0..."
-sed -i 's/bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# echo "Updating bind-address to 0.0.0.0..."
+# sed -i 's/bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+echo "WP_DATABASE: ${MYSQL_WP_DATABASE}"
+if [ -z "${MYSQL_WP_DATABASE}" ]; then
+    echo "No WP-Database specified. Exiting..."
+    exit 1
+fi
 
 if [ ! -d "/var/lib/mysql/${MYSQL_WP_DATABASE}" ]; then
     echo "Initializing Wordpress Database..."
-    service mariadb start
+    # service mariadb start
+
+    # Start MariaDB safely
+    echo "Starting MariaDB..."
+    mysqld_safe --datadir=/var/lib/mysql &
 
     echo "Waiting for MariaDB to fully start..."
-    while [ ! -S /run/mysqld/mysqld.sock ]; do
+    until mysqladmin ping --silent ; do
+        echo "Sleeping for 1 sec..."
         sleep 1
     done
     echo "MariaDB started successfully."
+
+    echo "Setting mysql root password..."
+    mysqladmin -u root password "${MYSQL_ROOT_PW}"
+
+    echo "Making sure that MariaDb is running after setting root password..."
+    until mysqladmin ping ; do
+        sleep 1
+    done
 
     echo "Creating Wordpress Database..."
     mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_WP_DATABASE};"
@@ -23,13 +42,10 @@ if [ ! -d "/var/lib/mysql/${MYSQL_WP_DATABASE}" ]; then
     echo "Granting Privileges for root..."
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_WP_DATABASE}.* TO 'root'@'localhost';"
     echo "Flushing Privileges..."
-    # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PW}';"
-    # mysql -uroot -p${MYSQL_ROOT_PW} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PW}';"
     mysql -e "FLUSH PRIVILEGES;"
-    echo "altering root password..."
-    mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PW}';"
-    echo "Shutting down Mariadb..."
-    mysqladmin -uroot --password=${MYSQL_ROOT_PW} shutdown
+
+    echo "Stopping MariaDB..."
+    mysqladmin -uroot -p"${MYSQL_ROOT_PW}" shutdown
 else
     echo "Wordpress Database already exists.";
 fi
