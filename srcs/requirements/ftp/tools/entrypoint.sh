b7621097733f24f6042756209db6dff928aee9d9
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2025-01-03 20:05:14 +0100

    feat: setting up ftp

--- srcs/requirements/ftp/tools/entrypoint.sh
+++ srcs/requirements/ftp/tools/entrypoint.sh
@@ -4,11 +4,11 @@ echo "Starting vsftpd..."
 # Create FTP user if not exists
 if ! id "$FTP_USER" >/dev/null 2>&1; then
     echo "Creating FTP user $FTP_USER..."
-    useradd -m -d /var/www/html -s /usr/sbin/nologin $FTP_USER
-    echo "$FTP_USER:1" | chpasswd
-    echo "$FTP_USER" >> /etc/vsftpd.userlist
-else
-    echo "FTP user $FTP_USER already exists. Skipping creation."
+    useradd -m -d /var/www/html $FTP_USER
 fi
+echo "$FTP_USER:$FTP_USER_PW" | chpasswd
+chown $FTP_USER:$FTP_USER -R /var/www/html
+echo "$FTP_USER" >> /etc/vsftpd.userlist
+echo "User $FTP_USER added to vsftpd.userlist"
 
 exec "$@"
